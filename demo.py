cd96922d3f1f91d26ba9a59724ace75aa1e38607
qilei123 <qileimail123@gmail.com>
2021-01-25 01:38:10 -0500

    test

--- demo.py
+++ demo.py
@@ -15,7 +15,7 @@ import datetime
 
 class ImageReader(object):
     def __init__(self, file_names):
-        
+        self.file_dir = file_names
         self.file_names = file_names
         print(file_names)
         if isinstance(file_names,str):
@@ -31,7 +31,8 @@ class ImageReader(object):
     def __next__(self):
         if self.idx == self.max_idx:
             raise StopIteration
-        img = cv2.imread(self.file_names[self.idx], cv2.IMREAD_COLOR)
+        #img = cv2.imread(self.file_names[self.idx], cv2.IMREAD_COLOR)
+        img = cv2.imread(os.path.join(self.file_dir,str(self.idx).zfill(5)+".jpg"), cv2.IMREAD_COLOR)
         if img.size == 0:
             raise IOError('Image {} cannot be read'.format(self.file_names[self.idx]))
         img_dir = self.file_names[self.idx]
