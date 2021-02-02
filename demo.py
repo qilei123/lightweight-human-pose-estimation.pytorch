3fcced7c28de490691dfe441c4391c32d947332b
qilei123 <qileimail123@gmail.com>
2021-02-02 17:31:10 -0500

    test

--- demo.py
+++ demo.py
@@ -158,7 +158,50 @@ def run_demo(net, image_provider, height_size, cpu, track, smooth):
                 delay = 1
         '''
 
-if __name__ == '__main__':
+
+class LightBody(object):
+    def __init__(self,model_path):
+        self.model = PoseEstimationWithMobileNet()
+        checkpoint = torch.load(model_path,map_location='cpu')
+        load_state(self.model, checkpoint)
+        self.model = self.model.eval()
+        self.model = self.model.cuda()
+
+    def __call__(self, img,height_size = 256):
+        stride = 8
+        upsample_ratio = 4
+        num_keypoints = Pose.num_kpts
+        orig_img = img.copy()
+        heatmaps, pafs, scale, pad = infer_fast(net, img, height_size, stride, upsample_ratio, cpu)
+    
+        total_keypoints_num = 0
+        all_keypoints_by_type = []
+        for kpt_idx in range(num_keypoints):  # 19th for bg
+            total_keypoints_num += extract_keypoints(heatmaps[:, :, kpt_idx], all_keypoints_by_type, total_keypoints_num)
+
+        pose_entries, all_keypoints = group_keypoints(all_keypoints_by_type, pafs)
+        for kpt_id in range(all_keypoints.shape[0]):
+            all_keypoints[kpt_id, 0] = (all_keypoints[kpt_id, 0] * stride / upsample_ratio - pad[1]) / scale
+            all_keypoints[kpt_id, 1] = (all_keypoints[kpt_id, 1] * stride / upsample_ratio - pad[0]) / scale
+        current_poses = []
+        for n in range(len(pose_entries)):
+            if len(pose_entries[n]) == 0:
+                continue
+            pose_keypoints = np.ones((num_keypoints, 2), dtype=np.int32) * -1
+            for kpt_id in range(num_keypoints):
+                if pose_entries[n][kpt_id] != -1.0:  # keypoint was found
+                    pose_keypoints[kpt_id, 0] = int(all_keypoints[int(pose_entries[n][kpt_id]), 0])
+                    pose_keypoints[kpt_id, 1] = int(all_keypoints[int(pose_entries[n][kpt_id]), 1])
+            pose = Pose(pose_keypoints, pose_entries[n][18])
+            current_poses.append(pose)
+        for pose in current_poses:
+            pose.draw(img)
+        img = cv2.addWeighted(orig_img, 0.6, img, 0.4, 0)
+        cv2.imwrite("/data1/qilei_chen/DEVELOPMENTS/test1.jpg",img)
+        return current_poses
+
+
+def main1():
     parser = argparse.ArgumentParser(
         description='''Lightweight human pose estimation python demo.
                        This is just for quick results preview.
@@ -189,3 +232,15 @@ if __name__ == '__main__':
         args.track = 0
 
     run_demo(net, frame_provider, args.height_size, args.cpu, args.track, args.smooth)
+
+def main2():
+    light_body =  LightBody("models/checkpoint_iter_370000.pth")
+    image = cv2.imread("/data2/qilei_chen/DATA/ShanghaiAutograding/gangganpingheng_images_240/15_1/right/23556.jpg")
+    for i in range(10):
+        start_time=datetime.datetime.now()
+        print(light_body(image))
+        end_time=datetime.datetime.now()
+        print((end_time-start_time).microseconds/1000)
+
+if __name__ == '__main__':
+    main1()
